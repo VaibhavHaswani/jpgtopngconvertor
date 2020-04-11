2649ddc43f9ed33a31526ddd1353c37cbe65d014
Vaibhav Haswani <vaibhavhaswani@gmail.com>
2020-04-11 18:23:04 +0530

    support for linux added

--- jpgtopngconv.py
+++ jpgtopngconv.py
@@ -5,15 +5,15 @@ import sys
 import os
 path=sys.argv[1]
 pngf=sys.argv[2]
-li=[f for f in os.listdir(path) if "." in f and f.split('.')[1]=="jpg"]
+li=[f for f in os.listdir(path) if os.path.isfile(os.path.join(path,f)) and f.split('.')[1]=="jpg"]
 
-npath=f"{path}\\{pngf}"
+npath=os.path.join(path,pngf)
 if not os.path.isdir(npath):   #check if directory exist
 	os.mkdir(npath)	
 
 for img in li:
-	jpg=Image.open(path+"\\"+img)
-	jpg.save(f"{path}\\{pngf}\\{img.split('.')[0]}.png","png")
+	jpg=Image.open(os.path.join(path,img))
+	jpg.save(os.path.join(npath,f"{img.split('.')[0]}.png"),"png")
 
 
 
